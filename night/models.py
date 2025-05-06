09c86b9c6f5514bdf814e4c811494e76dfa35fc3
ElFalch <eleanorfalch95@gmail.com>
2025-05-06 13:17:08 +0100

    feat: name post list items after post title

--- night/models.py
+++ night/models.py
@@ -15,6 +15,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     class Meta:
         ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title}"    
 
 # Review Model
 class Review(models.Model):
