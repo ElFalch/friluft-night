1843e0527d1ba8118cf18186cc0d31f5da3962d2
ElFalch <eleanorfalch95@gmail.com>
2025-05-07 14:46:35 +0100

    feat: update urls to include views

--- friluft/urls.py
+++ friluft/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from night import views as night_views
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("night.urls"), name="night-urls"),
     path('admin/', admin.site.urls),
-    path('night/', night_views.my_night, name='night'),
 ]
