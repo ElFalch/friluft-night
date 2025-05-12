67791c5d82f3ae388a4f91ef1b332e1a42a5ca65
ElFalch <eleanorfalch95@gmail.com>
2025-05-12 14:32:38 +0100

    fix: simplify admin.py

--- night/admin.py
+++ night/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post
-from .models import Review
+from .models import Post, Review
 from django_summernote.admin import SummernoteModelAdmin
 
 
