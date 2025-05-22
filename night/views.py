0e014bede2ecc456b4930b50d8bdc3c3a16d001f
ElFalch <eleanorfalch95@gmail.com>
2025-05-22 11:59:53 +0100

    feat: add post_detail.html with image

--- night/views.py
+++ night/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "night/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`night.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`night.Post`.
+
+    **Template:**
+
+    :template:`night/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "night/post_detail.html",
+        {"post": post},
+    )
