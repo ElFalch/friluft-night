a02060bdd298b15ef2578b986fc0da6bc62e8532
ElFalch <eleanorfalch95@gmail.com>
2025-06-03 12:33:21 +0100

    feat: add url for add_post

--- night/urls.py
+++ night/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
+    path("add_post/", views.add_post, name='add_post'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_review/<int:review_id>',
          views.review_edit, name='review_edit'),
