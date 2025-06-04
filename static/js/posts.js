ec6919e0bc5e19503fca55a3278d40e69954f9b8
ElFalch <eleanorfalch95@gmail.com>
2025-06-04 16:36:33 +0100

    fix: correct naming of JS elements

--- static/js/posts.js
+++ static/js/posts.js
@@ -1,7 +1,11 @@
-const posteditButtons = document.getElementsByClassName("btn-post-edit");
+const postEditButtons = document.getElementsByClassName("btn-post-edit");
 const postText = document.getElementById("id_content");
 const postForm = document.getElementById("postForm");
 const submitButton = document.getElementById("submitButton");
+
+const postDeleteModal = new bootstrap.Modal(document.getElementById("postdeleteModal"));
+const postDeleteButtons = document.getElementsByClassName("btn-post-delete");
+const postDeleteConfirm = document.getElementById("postdeleteConfirm");
 
 /**
 * Initializes edit functionality for the provided edit buttons.
@@ -13,7 +17,7 @@ const submitButton = document.getElementById("submitButton");
 * - Updates the submit button's text to "Update".
 * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
 */
-for (let button of posteditButtons) {
+for (let button of postEditButtons) {
   button.addEventListener("click", (e) => {
     let postId = e.target.getAttribute("post_id");
     let postContent = document.getElementById(`post${postId}`).innerText;
