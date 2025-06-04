9dbdeb83cb555af0d576d1e7b484e7ca411de417
ElFalch <eleanorfalch95@gmail.com>
2025-06-04 16:37:02 +0100

    feat: add JS loop for post deletion

--- static/js/posts.js
+++ static/js/posts.js
@@ -26,3 +26,21 @@ for (let button of postEditButtons) {
     commentForm.setAttribute("action", `edit_post/${postId}`);
   });
 }
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated posts's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific post.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of postDeleteButtons) {
+  button.addEventListener("click", (e) => {
+    let postId = e.target.getAttribute("post_id");
+    postDeleteConfirm.href = `delete_post/${postId}`;
+    postDeleteModal.show();
+  });
+}
