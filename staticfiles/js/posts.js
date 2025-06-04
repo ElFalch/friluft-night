13d1b0bca70224c4bfef994d3857f1c8fa4727b1
ElFalch <eleanorfalch95@gmail.com>
2025-06-04 16:38:00 +0100

    fix: collect static files

--- staticfiles/js/posts.js
+++ staticfiles/js/posts.js
@@ -1,7 +1,11 @@
-const posteditButtons = document.getElementsByClassName("btn-post-edit");
+const postEditButtons = document.getElementsByClassName("btn-post-edit");
 const postText = document.getElementById("id_content");
 const postForm = document.getElementById("postForm");
 const submitButton = document.getElementById("submitButton");
+
+const postDeleteModal = new bootstrap.Modal(document.getElementById("postdeleteModal"));
+const postDeleteButtons = document.getElementsByClassName("btn-post-delete");
+const postDeleteConfirm = document.getElementById("postdeleteConfirm");
 
 /**
 * Initializes edit functionality for the provided edit buttons.
@@ -13,12 +17,30 @@ const submitButton = document.getElementById("submitButton");
 * - Updates the submit button's text to "Update".
 * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
 */
-for (let button of posteditButtons) {
+for (let button of postEditButtons) {
   button.addEventListener("click", (e) => {
     let postId = e.target.getAttribute("post_id");
     let postContent = document.getElementById(`post${postId}`).innerText;
     postText.value = postContent;
     submitButton.innerText = "Update";
     commentForm.setAttribute("action", `edit_post/${postId}`);
+  });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated posts's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific post.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of postDeleteButtons) {
+  button.addEventListener("click", (e) => {
+    let postId = e.target.getAttribute("post_id");
+    postDeleteConfirm.href = `delete_post/${postId}`;
+    postDeleteModal.show();
   });
 }
